f62d8e99ad6f76aa9b99e49a94e4f12f8c474f30
root <root@capstone-api-app>
2023-11-13 20:32:42 +0000

    singular db

--- db.py
+++ db.py
@@ -19,16 +19,16 @@ engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(bind=engine)
 
 class ResetToken(Base):
-    __tablename__ = 'reset_tokens'
+    __tablename__ = 'reset_token'
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, index=True)
     token = Column(String, unique=True)
-    expires_at = Column(DateTime, server_default=text('(now() + interval 30 minute)'))
+    expires_at = Column(DateTime, server_default=text('(now() + interval \'30 minutes\')'))
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(255), index=True)
@@ -43,50 +43,34 @@ class User(Base):
 
 
 class Wallet(Base):
-    __tablename__ = "wallets"
+    __tablename__ = "wallet"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
     balance = Column(Float)
 
-class Card(Base):
-    __tablename__ = "cards"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("Rei Kuromiyausers.id"))
-    card_number = Column(String(255))
-
 class Car(Base):
-    __tablename__ = "cars"
+    __tablename__ = "car"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
     license_plate = Column(String(255))
     year = Column(Integer)  # Add the year field
     brand = Column(String(255))  # Add the brand field
     model = Column(String(255))  # Add the model field
 
     # Establish a many-to-one relationship with users
-    user = relationship("User", back_populates="cars")
-    parking_history = relationship("ParkingHistory", back_populates="car")
+    user = relationship("User", back_populates="car")
     is_active = Column(Boolean,default='true')
     in_use = Column(Boolean,default='false')
 
-class ParkingHistory(Base):
-    __tablename__ = "parking_history"
-
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)  # Add autoincrement=True here
-    car_id = Column(Integer, ForeignKey("cars.id"))
-    date = Column(DateTime(timezone=True), server_default=func.now())
-
-    car = relationship("Car", back_populates="parking_history")
 
 
 class ParkingMovement(Base):
-    __tablename__ = "parking_movements"
+    __tablename__ = "parking_movement"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
     entry_time = Column(DateTime, default=datetime.utcnow)
     exit_time = Column(DateTime, nullable=True)
     parking_spot_id = Column(String)
@@ -97,21 +81,21 @@ class ParkingMovement(Base):
     notes = Column(String)
 
     # Define relationships
-    user = relationship("User", back_populates="parking_movements")
+    user = relationship("User", back_populates="parking_movement")
 
 
 class Reservation(Base):
-    __tablename__ = "reservations"
+    __tablename__ = "reservation"
 
     id = Column(String, primary_key=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    parking_spot_id = Column(String)  # Si tienes una tabla para espacios de estacionamiento, podrías usar ForeignKey aquí también
+    user_id = Column(Integer, ForeignKey("user.id"))
+    parking_spot_id = Column(String)  
     start_time = Column(DateTime, nullable=False)
     end_time = Column(DateTime, nullable=False)
     is_active = Column(Boolean, default=True)
     
     # Relaciones
-    user = relationship("User", back_populates="reservations")
+    user = relationship("User", back_populates="reservation")
     # parking_spot = relationship("ParkingSpot", back_populates="reservations") # Descomenta esto si tienes una tabla de ParkingSpot
 
 # Agrega la relación en la clase User
